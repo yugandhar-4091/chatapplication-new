f13470aaae6eb50470f26ca0f2dd882c8593b4c3
yugandhar <yugandhar4091@gmail.com>
2025-11-20 19:22:14 +0530

    Fix asgi import order

--- chatproject/asgi.py
+++ chatproject/asgi.py
@@ -2,13 +2,19 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
+
+# Load Django before importing routing
+django_asgi_app = get_asgi_application()
+
+import chat.routing   # <-- Import AFTER Django setup
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    )
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
